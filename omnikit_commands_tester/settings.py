f1a2e386cbdaa8e6ecbf4c62930ca383f1861777
Eelke Jager <info@lytrix.com>
2018-11-27 21:47:58 +0100

    test removing sqlite3

--- omnikit_commands_tester/settings.py
+++ omnikit_commands_tester/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'omnikit_commands_tester.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 import dj_database_url
 
